d40a2daa090694f4262cc356eac1811643d008ac
Tyler Combs <tyler@tlcombs.com>
2024-07-24 11:41:11 -0500

    Initial Commit

---
+++ FloatingPanelUIHostingControllerBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatingPanelUIHostingControllerBug
+//
+//  Created by Tyler Combs on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
